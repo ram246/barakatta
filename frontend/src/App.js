e55f915d4b3a14e47feb1b734f18d7048f07a50a
Ram Gurram <abhi6ama@gmail.com>
2020-06-04 14:00:47 -0400

    added basic frontend app in react

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <table>
+      <td>
+        <tr>1</tr>
+        <tr>2</tr>
+      </td>
+      <td>
+        <tr>3</tr>
+        <tr>4</tr>
+      </td>
+    </table>
+  );
+}
+
+export default App;
