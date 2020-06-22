0e7060968d13d5a46db9a8f3c285c745ca41d557
Ram Gurram <abhi6ama@gmail.com>
2020-06-22 16:17:05 -0400

    added helper functions

--- frontend/src/util/util.js
+++ frontend/src/util/util.js
@@ -21,12 +21,40 @@ class Constants {
   // Cordinates are listed as strings first char being the x and second is y
   player_starts = ["", "14", "74", "47", "41"];
   // Max moves is 49 when there play has at least one kill
-  init_piece_data = {"onBoard": false, "completed": false, "cord": [0,0], "maxMove": 25};
-  
+  init_piece_data = {
+    onBoard: false,
+    completed: false,
+    cord: [0, 0],
+    maxMove: 25,
+  };
+}
+
+class GameRules {
+  /**
+   * This class contains helper functions to implement and check for game rules violations
+   */
+  playerCanMove(game, player, moves) {
+    /**
+     * Given the current (game) and (player) checks to make sure the player can make all the (moves)
+     * returns false if the player cannot make all those moves.
+     */
+    let sum = 0;
+    for (let i in moves) {
+      sum += i;
+    }
+    let sum_player = 0;
+    for (let i = 0; i < 6; i++) {
+      sum_player += game["piecesData"][player][i]["maxMove"];
+    }
+    if (sum_player >= sum) {
+      return true;
+    }
+  }
 }
 
 export let util = new Util();
 export let consts = new Constants();
+export let gameRules = new GameRules();
 
 export function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
